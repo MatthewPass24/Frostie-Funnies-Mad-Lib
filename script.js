7cabf0cdcfc0b61f1e31e45fba54b4716ca4a8f6
MatthewPass24 <matthewpass24@gmail.com>
2023-12-28 16:52:46 -0500

    finished story draft

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let adjective = prompt('Enter an Adjective:')
-let noun = prompt('Enter a noun:')
-let verb = prompt('Enter a verb:')
-let adverb = prompt ('Enter an adverb:')
 let number = prompt('Enter a number')
-let clothing = prompt('Enter an article of clothing ')
+let clothing = prompt('Enter an article of clothing')
+let noun = prompt('Enter a noun:')
+let adjective = prompt('Enter a adjective')
+let animal = prompt ('Enter an animal ')
+let dessert = prompt ('Enter a dessert')
+let movie = prompt ('Enter a Christmas movie')
 
-let story = `On a December morning, Jack and his brother Mason woke up and saw ${number} feet of snow. Wanting to play, they grabbed their ${clothing} and when they were outside, Mason called for his friends. Together they made a big ${noun} and deicded to have a ${adjective} snowball fight. ${adverb} ${verb}.`
+let story = `On a December morning, Jack and his brother Mason woke up and saw ${number} feet of snow. Wanting to play, they grabbed their ${clothing} and when they were outside, Mason called for his friends. Together they made a big ${noun} and decided to have a ${adjective} snowball fight. Suddenly a huge ${animal} came and destroyed their creation, sending them inside. Finally they had some hot chocolate with ${dessert} and watched ${movie} together.`
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
