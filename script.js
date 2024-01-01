fb9b8f8a73d0edac6af986c74cd8164abe9418c9
MatthewPass24 <matthewpass24@gmail.com>
2024-01-01 14:35:56 -0500

    added footer and changed story a bit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 document.getElementById('generateMadLib').addEventListener('click', function () {
+    let name = prompt('Enter a name')
+    let name2 = prompt('Enter a different name')
     let number = prompt('Enter a number')
     let clothing = prompt('Enter an article of clothing')
-    let noun = prompt('Enter a noun:')
-    let adjective = prompt('Enter a adjective')
+    let room = prompt('Enter a part of a house')
+    let adjective = prompt('Enter an adjective')
     let animal = prompt ('Enter an animal ')
     let dessert = prompt ('Enter a dessert')
     let movie = prompt ('Enter a Christmas movie')
-    let story = `On a December morning, Jack and his brother Mason woke up and saw ${number} feet of snow. Wanting to play, they grabbed their ${clothing} and when they were outside, Mason called for his friends. Together they built a big ${noun} and decided to have a ${adjective} snowball fight. Suddenly a huge ${animal} came and destroyed their creation, sending them inside. Finally they had some hot chocolate with ${dessert} and watched ${movie} together.`;
+    let story = `On a December morning, ${name} and their sibling ${name2} woke up and saw ${number} feet of snow. Wanting to play, they grabbed their ${clothing} and when they were outside, they called for the neighbors to come. Together they built a big snow fort with a huge${room} and decided to have a ${adjective} snowball fight.  Suddenly a huge ${animal} came and destroyed their fort, sending the kids inside. Finally they had some hot chocolate with ${dessert} and watched ${movie} together.`;
 
     let madLibOutputDiv = document.getElementById('madLibOutput');
     madLibOutputDiv.innerHTML = `<p>${story}</p>`;
